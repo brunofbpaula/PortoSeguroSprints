1e0ae46deca565a8666f7fe8497d79a41bf244bb
brunofbpaula <deepcooper@outlook.com>
2023-10-18 00:47:25 -0300

    Add connection

--- Connection/oracle.py
+++ Connection/oracle.py
@@ -1,1 +1,15 @@
-# Nada ainda
+import cx_Oracle
+import json
+
+
+# Pegando as credenciais
+with open('../credentials.txt', "r") as arquivo:
+    credentials = json.load(arquivo)
+login = credentials["Login"]
+senha = credentials["Password"]
+
+# Conectando ao banco de dados ORACLE
+dsn = cx_Oracle.makedsn(host="oracle.fiap.com.br", port=1521, sid="ORCL")
+connection = cx_Oracle.connect(user=login, password=senha, dsn=dsn)
+
+
