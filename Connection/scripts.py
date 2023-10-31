9f18c92926f1ac7eb0279a7e44d0324920724fed
brunofbpaula <deepcooper@outlook.com>
2023-10-31 15:31:03 -0300

    Test sequence

--- Connection/scripts.py
+++ Connection/scripts.py
@@ -3,28 +3,30 @@ from pandas import to_datetime
 from Backstage.functions import delay
 
 
-def scripts_cliente(comando, lista=None, id=None, variavel=None):
+def scripts_cliente(comando, lista=None, nr_cpf=None, variavel=None):
     """
     Função que executa um comando de data modeling na tabela T_POR_CLIENTE.
     :param variavel: Variável a ser atualizada no comando UPDATE.
     :param comando: Comando de data modeling.
     :param lista: Lista de valores do cliente.
-    :param id: Identificação do cliente.
+    :param nr_cpf: CPF do cliente.
     :return: None ou lista de valores do SELECT.
     """
+    comando.upper()
 
     if comando == 'INSERT':
         oracle.insert('INSERT INTO T_POR_CLIENTE(id_cliente, nr_cpf, nome_completo, idade, dt_nascimento, genero)'
-                      'VALUES (:id_cliente, :nr_cpf, :nome_completo, :idade, :dt_nascimento, :genero)', lista)
+                      'VALUES (T_POR_CLIENTE_seq.nextval, :nr_cpf, :nome_completo, '
+                      ':idade, :dt_nascimento, :genero)', lista)
 
     elif comando == 'DELETE':
-        oracle.delete('DELETE FROM T_POR_CLIENTE WHERE id_cliente = :id_cliente', id)
+        oracle.delete('DELETE FROM T_POR_CLIENTE WHERE nr_cpf = :nr_cpf', nr_cpf)
 
     elif comando == 'UPDATE':
-        oracle.update(f'UPDATE T_POR_CLIENTE SET {variavel} = :v WHERE id_cliente = :id_cliente', lista)
+        oracle.update(f'UPDATE T_POR_CLIENTE SET {variavel} = :v WHERE nr_cpf = :nr_cpf', lista)
 
     elif comando == 'SELECT':
-        values = oracle.select('SELECT * FROM T_POR_CLIENTE WHERE id_cliente = ', id)
+        values = oracle.select('SELECT * FROM T_POR_CLIENTE WHERE id_cliente = ', nr_cpf)
         return values
 
     else:
@@ -40,7 +42,7 @@ if __name__ == "__main__":
     lipe = [9, 11732549742, 'Felipe de Almeida Cardoso', 19, data_nascimento, 'Homem']
     scripts_cliente('INSERT', lista=lipe)
     delay(5)
-    scripts_cliente('DELETE', id=9)
+    scripts_cliente('DELETE', nr_cpf=11732549742)
 
     # Testando UPDATE
     data_nascimento = to_datetime('2005-05-09')
@@ -50,3 +52,9 @@ if __name__ == "__main__":
     delay(5)
     atualizar = ['Homem', miguel[0]]
     scripts_cliente('UPDATE', lista=atualizar, variavel='genero')
+
+    # Testando sequence
+    data_nascimento = to_datetime('1992-10-22')
+    savage = [87342579091, 'Shéyaa Bin Abraham-Joseph', 31, data_nascimento, 'Homem']
+    scripts_cliente('INSERT', lista=savage)
+    scripts_cliente('DELETE', nr_cpf=savage[0])
